9a61644a4a2137c77d3c1a49c1b96fe744c1ea7c
omgshalihin <shalihinsgp@gmail.com>
2023-03-20 14:53:06 +0100

    order producer successfully sends an event message to kafka broker topic

---
+++ orderService/src/main/java/com/example/orderService/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.example.orderService.controller;
+
+import com.example.baseDomains.dto.Order;
+import com.example.baseDomains.dto.OrderEvent;
+import com.example.orderService.kafka.OrderProducer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+    private OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("/orders")
+    public String placeOrder(@RequestBody Order order) {
+
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("Order status is in pending state");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+
+        return "Order placed successfully ...";
+    }
+}
